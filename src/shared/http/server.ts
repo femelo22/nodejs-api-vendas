005686dda6a29977a9c39d858ffdccf6268af89d
femelo22 <luiz123jfmg@gmail.com>
2022-08-26 15:32:49 -0300

    feat: adicioandoum middleware para execões

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,33 @@
-import express from "express";
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //liberar cors
+app.use(express.json()); //usar json
 
-app.use(routes);
+app.use(routes); // deixar rodas nossas rotas disponiveis
+
+//capturar excessoes do nosso sistema (middleware)
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Erro não identificado pelo sistema.',
+    });
+  },
+);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000! 🚀')
+  console.log('Server is running on port 3000! 🚀');
 });
-
