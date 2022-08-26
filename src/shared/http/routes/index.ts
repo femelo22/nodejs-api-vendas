a2c38cfda3a4abf05124915579aa1fc1b96cdb73
femelo22 <luiz123jfmg@gmail.com>
2022-08-26 15:13:07 -0300

    feat: Primeira rota, hello dev!

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,10 @@
+//Faz a chamada para todos as outras rotas do sistema
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Dev!!' })
+});
+
+export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('Começando projeto!');
+import express from "express";
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000! 🚀')
+});
+
